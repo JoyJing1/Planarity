1feb06e58a02d74ce45f6c59d6283914f4365658
joy <joy.jing1@gmail.com>
2016-07-11 20:13:28 -0700

    Remove comments & debuggers

--- js/bundle.js
+++ js/bundle.js
@@ -323,43 +323,31 @@
 	};
 	
 	GameView.prototype.render = function() {
-	  // const $board = $("<ul class='board'></ul>");
 	  this.ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
 	
 	  this.game.edges.forEach( (edge, i) => {
 	    edge.draw(this.ctx);
-	    // console.log(`edge ${i}`);
-	    // console.log(`vertex1: (${edge.vertex1.x}, ${edge.vertex1.y})`);
-	    // console.log(`vertex2: (${edge.vertex2.x}, ${edge.vertex2.y})`);
 	  });
 	
 	  this.game.vertices.forEach( (vertex, i) => {
 	    vertex.draw(this.ctx);
 	  });
-	
-	  // this.root.append($board);
 	};
 	
 	GameView.prototype.bindEvents = function() {
 	  $("canvas").on("mousedown", event => {
-	    // console.log("inside mousedown on .canvas");
 	
 	    this.game.vertices.forEach( vertex => {
 	      const dist = Util.distFromMouse(vertex, event);
-	      // console.log(vertex);
-	      // console.log(dist);
 	
 	      if (dist < 100) {
 	        vertex.selected = true;
-	        // console.log(`selected vertex ${vertex.index}`);
 	      }
 	    });
 	
 	  });
 	
 	  $("canvas").on("mouseup", event => {
-	    // Change all statuses to not selected
-	    // debugger;
 	    this.game.vertices.forEach( vertex => {
 	      vertex.selected = false;
 	    });
@@ -368,25 +356,20 @@
 	  $(document).mousemove( event => {
 	    const yAdjust = -65;
 	    const xAdjust = -8;
+	
 	    this.currentMousePos.x = event.pageX + xAdjust;
 	    this.currentMousePos.y = event.pageY + yAdjust;
-	    // console.log(this.currentMousePos);
 	  });
 	
 	};
 	
 	GameView.prototype.follow = function(game, currentMousePos) {
-	  // debugger;
-	  // console.log("inside GameView.follow()");
 	  game.vertices.forEach( vertex => {
 	    if (vertex.selected) {
-	      // debugger;
 	      vertex.x = currentMousePos.x;
 	      vertex.y = currentMousePos.y;
-	      // console.log(`New position for vertex ${vertex.index} is (${vertex.x}, ${vertex.y})`);
 	    }
 	  });
-	
 	
 	};
 	
